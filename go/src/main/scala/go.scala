b11909e24a24363a7c19b8de558542db0cc97a07
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-21 16:15:37 +0530

    c

--- go/src/main/scala/go.scala
+++ go/src/main/scala/go.scala
@@ -7,6 +7,9 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 object go {
   
@@ -24,8 +27,14 @@ object go {
     
     val fs = FileSystem.get(sc.hadoopConfiguration)
     
-    //val dirSize = fs.getContentSummary(/user/hive/warehouse/hj_t_item_master).getLength
+    
+    
+    val dirSize = fs.getContentSummary(/user/hive/warehouse/hj_t_item_master).getLength
     //val fileNum = dirSize/(5 * 1024 * 1024)  
+    
+    
+    //val textFile = sc.textFile("hdfs://input/war-and-peace.txt")
+
 
      
    // val sqlContext = new org.apache.spark.sql.SQLContext(sc)
@@ -34,7 +43,7 @@ object go {
     
     val dfProc = hiveContext.sql("select * from antuit_stage.hj_t_item_master")
     
-    dfProc.coalesce(10).write.format("orc").saveAsTable("default.hj_t_bmm_dummy")
+    dfProc.coalesce(10).write.saveAsTable("default.hj_t_bmm_dummy")
 
 
    } 
