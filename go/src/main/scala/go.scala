445ec9856d23c1a3b77443899ac2aaea3a73623e
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-21 19:22:28 +0530

    update

--- go/src/main/scala/go.scala
+++ go/src/main/scala/go.scala
@@ -20,31 +20,41 @@ object go {
     val conf = new SparkConf().setAppName("MRS Import")
     val sc = new SparkContext(conf)
     
+    val dbtable = args(0)
+   	val table = dbtable.substring(dbtable.indexOf(".")+1)
+		val db = dbtable.substring(0,dbtable.indexOf("."))
+    
+		val sourceTable =  table.substring(3)
+    
     //sc.hadoopConfiguration.setInt( "mapreduce.input.fileinputformat.split.minsize",5242880)
     //sc.hadoopConfiguration.setInt( "mapreduce.input.fileinputformat.split.maxsize",5242880)
     
     import org.apache.spark.SparkContext._
     
-    val fs = FileSystem.get(sc.hadoopConfiguration)
-    
-    
-    
-   // val dirSize = fs.getContentSummary(/user/hive/warehouse/hj_t_item_master).getLength
-    //val fileNum = dirSize/(5 * 1024 * 1024)  
-    
-    
-    //val textFile = sc.textFile("hdfs://input/war-and-peace.txt")
-
-
-     
-   // val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    
     val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
+   
+        val hjSource = hiveContext.load("jdbc", 
+  Map(
+  "driver" -> "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+  "url" -> "jdbc:sqlserver://192.168.100.223:1433;database=AAD",
+  "user" -> "readonly",
+  "password" -> "HJ#ric1!",
+  "dbtable" -> sourceTable))
+      
+   val DF = hiveContext.sql("SELECT * FROM antuit_stage."+table)
+   //val OldDF = hiveContext.sql("SELECT * FROM antuit_stage."+table)
+  
+    val DF1=DF.drop("sha2")
     
-    val dfProc = hiveContext.sql("select * from antuit_stage.hj_t_item_master")
+    val DF2=DF1.drop("sequence")
     
-    dfProc.coalesce(10).write.saveAsTable("default.hj_t_bmm_dummy")
-
+    val oldDF=DF2.drop("archive_date")
+    
+    hjSource.write.saveAsTable("default.hj_source_item_master")
+    
+    val upsertsDF = hjSource.except(oldDF)
+    
+    upsertsDF.write.saveAsTable("default.upserts_item_master")
 
    } 
 }
