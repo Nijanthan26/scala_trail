9c35792b399acd0beaf61d4045768a14f20df7f7
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-21 16:02:24 +0530

    coalles

--- go/src/main/scala/go.scala
+++ go/src/main/scala/go.scala
@@ -5,6 +5,8 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.sql._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.hive.HiveContext
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 object go {
   
@@ -15,17 +17,24 @@ object go {
     val conf = new SparkConf().setAppName("MRS Import")
     val sc = new SparkContext(conf)
     
-    sc.hadoopConfiguration.setInt( "mapreduce.input.fileinputformat.split.minsize",5242880)
-    sc.hadoopConfiguration.setInt( "mapreduce.input.fileinputformat.split.maxsize",5242880)
+    //sc.hadoopConfiguration.setInt( "mapreduce.input.fileinputformat.split.minsize",5242880)
+    //sc.hadoopConfiguration.setInt( "mapreduce.input.fileinputformat.split.maxsize",5242880)
     
     import org.apache.spark.SparkContext._
+    
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    
+    val dirSize = fs.getContentSummary(/user/hive/warehouse/hj_t_item_master).getLength
+    val fileNum = dirSize/(5 * 1024 * 1024)  
+
+     
    // val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     
     val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     
     val dfProc = hiveContext.sql("select * from antuit_stage.hj_t_item_master")
     
-    dfProc.write.format("orc").saveAsTable("default.hj_t_bmm_dummy")
+    dfProc.coalesce(fileNum).write.format("orc").saveAsTable("default.hj_t_bmm_dummy")
 
 
    } 
