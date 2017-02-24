faaaba4ae722b13980e69b9cef16de0e9b8ae53e
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-24 14:54:19 +0530

    sdc

--- go/src/main/scala/mrsfirstdu.scala
+++ go/src/main/scala/mrsfirstdu.scala
@@ -93,7 +93,7 @@ val SourcSeq = source.drop("sequence")
 		
 		val res = srcDelta.except(SourcSeq)
 		
-		res.saveAsTable("accelos.adj_trn_temp")
+		res.write.saveAsTable("accelos.adj_trn_temp")
 		
 	
 		
