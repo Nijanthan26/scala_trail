6ef0ad54c5439c79b31d8eeac9b596c03500c35d
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-24 14:13:35 +0530

    update

--- go/src/main/scala/mrsfirstdu.scala
+++ go/src/main/scala/mrsfirstdu.scala
@@ -3,7 +3,7 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql._
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
@@ -26,6 +26,7 @@ object mrsfirstdu {
 		val conf = new SparkConf().setAppName("MRS")
 		val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+		val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     val dbtable = args(0)
    	val table = dbtable.substring(dbtable.indexOf(".")+1)
 		val db = dbtable.substring(0,dbtable.indexOf("."))
@@ -38,6 +39,9 @@ option("dbtable", "adj_trn").
 option("user", "readonly").
 option("password", "R3@60n1Y$").load() 
 
+val source=hiveContext.sql("select * from anutit_stage.mrs15_adj_trn")
+
+ val SourcSeq = source.drop("sequence")
 		 /*
     val mrsSource09 = sqlContext.read.format("jdbc").options( 
   Map(
@@ -81,9 +85,13 @@ option("password", "R3@60n1Y$").load()
    
 		val res = addDeltaFirstTime(mrsDf1)
 		
+		import org.apache.spark.sql.types._
+    val schema = StructType(SourcSeq.schema.fields)
+		var dfDeltatx = sqlContext.createDataFrame(mrsSource09,schema )
 		
 		//res.write.mode("append").format("orc").option("delimiter", "\t").save("/antuit/sqoopdest/mrs15_adj_trn"); 
-		res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "|").save("/antuit/sqoopdest/mrs15_adj_trn");
+		//dfDeltatx.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "|").save("/antuit/sqoopdest/mrs15_adj_trn");
+		dfDeltatx.write.mode("overwrite").saveAsTable("accelos.adj_trn_test");
 
   }
   
