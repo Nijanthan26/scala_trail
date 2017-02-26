ca24818910c2baaf4d93f314de25be25f7ed08a2
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-26 23:18:15 +0530

    yes

--- go/src/main/scala/new_go_fd.scala
+++ go/src/main/scala/new_go_fd.scala
@@ -59,7 +59,7 @@ option("password", "R3@60n1Y$").load()
        
    
 		val res = addDeltaFirstTime(mrsDf1)
-    res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").quoteMode(none).save("/antuit/databases/testwrite3/"+table);
+    res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").option("quoteMode", "NONE").save("/antuit/databases/testwrite3/"+table);
 
 
   }
