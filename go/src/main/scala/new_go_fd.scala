9229c0aab4c4544860a0e645241b99eb1b1d7167
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-27 11:38:31 +0530

    s

--- go/src/main/scala/new_go_fd.scala
+++ go/src/main/scala/new_go_fd.scala
@@ -63,7 +63,7 @@ option("password", "R3@60n1Y$").load()
 		val res = addDeltaFirstTime(mrsDf1)
 		res.registerTempTable("mrs_test_data")
 		
-		sqlContext.sql("insert overwrite accelos."+table+" select * from  mrs_test_data")
+		sqlContext.sql("insert overwrite table accelos."+table+" select * from  mrs_test_data")
   //  res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").option("quoteMode", "NONE").option("escape", "\\").save("/antuit/databases/testwrite3/"+table);
 		//res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").option("quote", " ").save("/antuit/databases/testwrite3/"+table);
 		//res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "|").save("/antuit/databases/testwrite3/"+table);
