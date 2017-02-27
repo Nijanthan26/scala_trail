47fb069d992c8221160e788a1c4602ea135a0f30
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-02-27 13:45:39 +0530

    s

--- go/src/main/scala/new_go_fd.scala
+++ go/src/main/scala/new_go_fd.scala
@@ -64,7 +64,7 @@ option("password", "R3@60n1Y$").load()
 		//res.registerTempTable("mrs_test_data")
 		
 		//sqlContext.sql("insert overwrite table `accelos.mrs15_adj_trn` select * from  `mrs_test_data`")
-  res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").option("quoteMode", "NONE").option("escape", "NONE").save("/antuit/databases/testwrite3/"+table);
+  res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").option("quoteMode", "NONE").option("escape", "\u0001").save("/antuit/databases/testwrite3/"+table);
 		//res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "\u0001").option("quote", " ").save("/antuit/databases/testwrite3/"+table);
 		//res.write.mode("overwrite").format("com.databricks.spark.csv").option("delimiter", "|").save("/antuit/databases/testwrite3/"+table);
 
