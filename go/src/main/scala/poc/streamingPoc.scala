bff902cbbe29586d2ef14080543d0d3b1e7adeb8
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-05-23 19:57:27 +0530

    update

--- go/src/main/scala/poc/streamingPoc.scala
+++ go/src/main/scala/poc/streamingPoc.scala
@@ -8,7 +8,7 @@ object streamingPoc {
   
 val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
 val ssc = new StreamingContext(conf,Seconds(1))
-val lines = ssc.SocketTextStream("localhost",9999)
+val lines = ssc.socketTextStream("localhost",9999)
 val words = lines.flatMap(_.split(" "))
 val pairs = words.map(word => (word,1))
 val wordCounts = pairs.reduceBykey(_+_)
