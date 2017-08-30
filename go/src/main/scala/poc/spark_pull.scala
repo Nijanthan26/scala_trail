3dd2d64f4db6ea0d9a9d9bbe13227e2361383aa7
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-08-30 09:20:05 +0530

    spark POC code

---
+++ go/src/main/scala/poc/spark_pull.scala
@@ -0,0 +1,34 @@
+package poc
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
+import java.security.MessageDigest
+import org.apache.spark.sql.Dataset
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import scala.reflect.runtime.universe
+import java.util.Calendar
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.functions.lit
+
+object spark_pull {
+  
+  def main(args: Array[String]): Unit = {
+    
+    val conf = new SparkConf().setAppName("Spark_POC")
+		val sc = new SparkContext(conf)
+		val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc) 
+    
+    val SourceData = hiveContext.read.format("jdbc").
+		option("url", "jdbc:mysql://172.31.6.118:3306/testdb").
+		option("driver", "com.mysql.jdbc.Driver").
+		option("dbtable", "table1").
+		option("user", "testuser").
+		option("password", "abdul").load()
+		
+		SourceData.show
+    
+  }
+  
+}
