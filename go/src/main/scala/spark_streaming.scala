6f7513882a086824b17f438f2b2200800b4fab9a
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-08-30 18:36:53 +0530

    object

--- go/src/main/scala/spark_streaming.scala
+++ go/src/main/scala/spark_streaming.scala
@@ -16,7 +16,7 @@ import org.apache.spark.sql.Row
 import org.apache.log4j.{ Level, Logger }
 import io.confluent.kafka.serializers.KafkaAvroDecoder
 
-object ConsumerTest extends App {
+object spark_streaming extends App {
 
   val conf = new SparkConf().setAppName("kafka-consumer")
   val sc = new SparkContext(conf)
