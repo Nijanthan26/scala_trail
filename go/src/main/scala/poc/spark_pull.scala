1cf1732d1b3c381dd4c2917e877d88f121672b97
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-09-01 12:01:10 +0530

    update

--- go/src/main/scala/poc/spark_pull.scala
+++ go/src/main/scala/poc/spark_pull.scala
@@ -17,9 +17,12 @@ object spark_pull {
   def main(args: Array[String]): Unit = {
     
     val conf = new SparkConf().setAppName("Spark_POC")
+    // Initializing Spark Context, heart of the program which loads all the required drivers and config files
 		val sc = new SparkContext(conf)
+    // Createing HiveContext out of spark context to make connection to Hive Metastore.
 		val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc) 
     
+    // Creating JDBC connection to RDBMS using hive Context and pulling required table
     val SourceData = hiveContext.read.format("jdbc").
 		option("url", "jdbc:mysql://172.31.6.118:3306/testdb").
 		option("driver", "com.mysql.jdbc.Driver").
@@ -27,21 +30,24 @@ object spark_pull {
 		option("user", "testuser").
 		option("password", "abdul").load()
 		
+		// Display the schema and data in the dataframe
 		//SourceData.show
 		
+		// Register the dataframe as temporary table, so that we can query the data in dataframe.
 		//SourceData.registerTempTable("source_table")
 		
 		//hiveContext.sql("insert into default.sqooptable select * from source_table")
 		
+		//select data from hive table in Datalake using Hive Context
 		val DL = hiveContext.sql("select * from default.sqooptable")
 		
+		// Perform except between two different dataframe ( source df & datalake Df) to get the updated and newly added records in source df
 		val updates=SourceData.except(DL)
 		
 		updates.registerTempTable("update_table")
 		
 		hiveContext.sql("create table default.dummy like default.sqooptable")
 		hiveContext.sql("insert into default.dummy select * from update_table")
-		
 		hiveContext.sql("insert into default.sqooptable select * from update_table")
 		hiveContext.sql("drop table if exists default.dummy")
   
