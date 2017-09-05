5fc3afecaa9c889ffdb4bf4a16c27ae2f11accdf
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-09-05 17:27:48 +0530

    comment logging

--- go/src/main/scala/spark_streaming.scala
+++ go/src/main/scala/spark_streaming.scala
@@ -10,7 +10,7 @@ import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.sql.types.{ StructType, StructField, StringType }
-import org.apache.spark.Logging
+//import org.apache.spark.Logging
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 import org.apache.log4j.{ Level, Logger }
